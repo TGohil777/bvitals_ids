420d1061587dfed264d972904e9abf3730aa4fb3
kiranyadavXA <kiran.yadav@crossasyst.com>
2019-08-28 17:06:37 +0530

    BV-001-pre: Fix project structure , added sequelize connect BV-001-pre: Implemented env file for development

---
+++ config/config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+console.log(process.env.NODE_ENV);
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
+    operatorsAliases: false
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
+    operatorsAliases: false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
+    operatorsAliases: false
+  }
+}
