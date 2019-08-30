1cacd8778ea1caff267a8b817c56a2527886e30d
kiranyadavXA <kiran.yadav@crossasyst.com>
2019-08-30 11:53:34 +0530

    Created auth table

---
+++ models/auth.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const auth = sequelize.define('auth', {
+    authid: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+    },
+    firstname: {
+      type: DataTypes.STRING(200),
+      allowNull: false
+    },
+    
+    lastname: {
+      type: DataTypes.STRING(200),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(200),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(200),
+      allowNull: false
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    deleted: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      }
+
+  }, { });
+  auth.associate = function(models) {
+
+
+  };
+  return auth;
+};
